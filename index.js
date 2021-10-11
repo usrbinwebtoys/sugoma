37300aa66d6116bec3d183f1778c84c443d6ef32
Sugoma <sug@sugomadi.cc>
2021-10-10 20:32:50 -0400

    yeetus

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
     $("body").on("click", function (e) {
-        $(this)
-            .append(
-                $("<img />").attr({
+        $(this).append(
+            $("<img />")
+                .attr({
                     id: "boomcircle",
                     src: "redcirlce.png",
                     style: `position: absolute; left: ${
                         e.pageX - 112.5
                     }px; top: ${e.pageY - 112.5}px`,
                 })
-            )
-            .fadeIn(100)
-            .delay(10000)
-            .fadeOut(1000);
+                .hide()
+                .fadeIn(100)
+                .delay(10000)
+                .fadeOut(1000)
+        );
         new Audio("vineboom.mp3").play();
     });
 });
