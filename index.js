7ceeac690c553f9a65aa2f5a303b8d5fcc9ac16f
Sugoma <sug@sugomadi.cc>
2021-10-10 20:21:04 -0400

    yeetus

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
             $("<img />").attr({
                 id: "boomcircle",
                 src: "redcirlce.png",
-                style: `position: absolute; left: ${e.pageX + 112.5}px; top: ${
-                    e.pageY + 112.5
+                style: `position: absolute; left: ${e.pageX - 112.5}px; top: ${
+                    e.pageY - 112.5
                 }px`,
             })
         );
