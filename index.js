692dcaf58f94408dee5412e10233e39973c9f0b4
Sugoma <sug@sugomadi.cc>
2021-10-10 21:18:42 -0400

    yeetus

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+    $(this).append(
+        $("<video loop/>")
+            .attr({
+                id: "vjo",
+                src: "",
+                width: 320,
+                height: 240,
+                type: "video/mp4",
+                style: "position: fixed; bottom: 0; left: 0;",
+            })
+            .hide()
+            .fadeIn(100)
+    );
     $("body").on("click", function (e) {
         $(this).append(
             $("<img />")
@@ -17,20 +30,8 @@ $(document).ready(function () {
                 })
         );
 
-        if (!$("#rock").length) {
-            $(this).append(
-                $("<video autoplay/>")
-                    .attr({
-                        id: "rock",
-                        src: "dwayne.mp4",
-                        width: 320,
-                        height: 240,
-                        type: "video/mp4",
-                        style: "position: fixed; bottom: 0; left: 0;",
-                    })
-                    .hide()
-                    .fadeIn(100)
-            );
+        if ($("#vjo").attr("src") === "") {
+            $("#vjo").attr("src", "dwayne.mp4")[0].play();
         }
 
         new Audio("vineboom.mp3").play();
